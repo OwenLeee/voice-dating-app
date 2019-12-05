54fb64bfa591f3ebfdf2b2c0e585fd5a8ab424a2
Harry Chow <harryccpongg@gmail.com>
2019-12-05 13:17:53 +0800

    basic express app setting

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-console.log('hello');
+import * as express from 'express';
+import { Request, Response } from 'express';
+import * as bodyParser from 'body-parser';
+import * as expressSession from 'express-session';
+import * as path from 'path';
+// import * as socketIO from 'socket.io';
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(expressSession({
+    secret: 'Tecky Academy teaches typescript',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req: Request, res: Response) {
+    res.end("Hello World");
+})
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}/`);
+});
