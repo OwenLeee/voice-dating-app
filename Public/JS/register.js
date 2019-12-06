3d303445a9713dbdb00ffeb42cf835ea07259ba2
Owen Lee <owen55889@gmail.com>
2019-12-06 17:07:07 +0800

    added login and register html

---
+++ Public/JS/register.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+    main();
+  };
+  
+  function main() {
+    const loginForm = document.getElementById("registerForm");
+    loginForm.addEventListener('submit', async function (e) {
+      e.preventDefault();
+
+      const email = loginForm.querySelector("input[name='email']").value;
+      const password = loginForm.querySelector("input[name='password']").value;
+
+      console.log(email, password);
+
+      const res = await fetch('/user/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+//                               !! should delete, pls check!! 
+    //   console.log(res.status);
+    //   if (res.status === 200) {
+    //     window.location.href = '/home.html';
+    //   } else {
+    //     const resJson = await res.json();
+    //     alert(resJson.message);
+    //   }
+    // })
+  
+  })};
+
+  
