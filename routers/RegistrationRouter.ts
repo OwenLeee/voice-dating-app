fe41a17dcf38de815346b7ea3a459d4c3abc4e75
Sherman Tam <shermantam0701@gmail.com>
2019-12-08 19:20:37 +0800

    update registration part

--- routers/RegistrationRouter.ts
+++ routers/RegistrationRouter.ts
@@ -1,24 +1,42 @@
 import * as express from "express";
+import * as multer from "multer";
+import * as path from "path";
 import { Request, Response } from "express";
 import { RegistrationService } from "../services/RegistrationService";
+
 
 export class RegistrationRouter {
 
     constructor(private registrationService: RegistrationService) { }
-
+    
     router() {
         const router = express.Router();
-        router.post('/', this.uploadInfo);
+
+        const storage = multer.diskStorage({
+            destination: "./pic-test",
+            filename: function (req, file, cb) {
+                cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+            }
+        });
+
+        const upload = multer({ dest:'pic-test/', storage: storage });
+
+        router.post('/uploadInfo', upload.single("uploadIcon"), this.uploadInfo);
+        
         return router;
     }
 
     private uploadInfo = async (req: Request, res: Response) => {
         try {
             if (req.user) {
-                res.json(await this.registrationService.uploadInfo);
+                await this.registrationService.uploadInfo(req.user["id"], req.body.uploadInfo);
+                res.json({ result: true });
+                res.send({ret_code: '0'});
+                console.log(req.file);
             }
         } catch (error) {
-            throw error;
+            res.status(404).json({ result: false });
+            console.log(error.message);
         }
     }
 }
--- services/RegistrationService.ts
+++ services/RegistrationService.ts
@@ -1,4 +1,4 @@
-import * as Knex from "Knex";
+import * as Knex from "knex";
 import * as Table from "../table";
 
 interface UploadInfo {
@@ -12,38 +12,29 @@ interface UploadInfo {
 
 export class RegistrationService {
 
-    constructor(private Knex: Knex) { };
+    constructor(private knex: Knex) { };
 
-    public async findLoginUser(email: string) {
-        try {
-            // SELECT email from "user" where email = 'sherman@sherman.com'
-            const users = await this.Knex.raw(/*SQL*/ `SELECT email from "${Table.USER}" where email = ?`, [email]);
-            return users.user_id;
-        }
-        catch (err) {
-            throw err;
-        }
-
-    }
-
-    async uploadInfo (uploadInfo: UploadInfo) {
+    async uploadInfo (user_id:number, uploadInfo: UploadInfo) {
+        console.log(user_id, uploadInfo);
         try {
             //INSERT INTO user_info (user_id, name, gender, date_of_birth, icon) VALUES (5, 'apple', 'female', '1995-10-01', 'apple.jpg');
-            await this.Knex.raw (/*SQL*/ `INSERT INTO "${Table.USER_INFO}" (name, gender, date_of_birth, icon) VALUES (?, ?, ?, ?, ?)`, [uploadInfo.name, uploadInfo.gender, uploadInfo.date_of_birth, uploadInfo.icon]);
-            await this.Knex.raw (/*SQL*/ `INSERT INTO "${Table.VOICE}" (voice_path) VALUES (?)`, [uploadInfo.voice_path]);
-            await this.Knex.raw (/*SQL*/ `INSERT INTO "${Table.PICTURE}" (picture_path) VALUES (?)`, [uploadInfo.picture_path]);
+            await this.knex.raw (/*SQL*/ `INSERT INTO "${Table.USER_INFO}" (user_id, name, gender, date_of_birth, icon) VALUES (?, ?, ?, ?, ?)`, [user_id, uploadInfo.name, uploadInfo.gender, uploadInfo.date_of_birth, uploadInfo.icon]);
+            await this.knex.raw (/*SQL*/ `INSERT INTO "${Table.VOICE}" (user_id, voice_path) VALUES (?, ?)`, [user_id, uploadInfo.voice_path]);
+            await this.knex.raw (/*SQL*/ `INSERT INTO "${Table.PICTURE}" (user_id, picture_path) VALUES (?, ?)`, [user_id, uploadInfo.picture_path]);
             return true;
         }
         catch (err) {
+            console.log(err);
             return false;
-            ;
         }
     }
 
 }
 
+// const knexConfig = require("../knexfile");
+// const knex = Knex(knexConfig[process.env.NODE_ENV || "development"]);
 
+// const registrationService = new RegistrationService(knex);
 // (async() => {
-//       const userService = new UserService(path.join(__dirname, "../json"));
-//     console.log(await userService.findLoginUser("admin", "admin"));
+//       console.log(await registrationService.uploadInfo(3, {name: "", gender: "", date_of_birth: new Date, voice_path: "", icon: "", picture_path: ""}));
 // })()
